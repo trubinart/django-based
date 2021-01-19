ed8e3f9084d1f02b2a38d318b97307d2f4c83139
truart <trubinart@gmail.com>
2021-01-19 18:33:09 +0300

    hw1 done

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mainapp import views as url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', url.index),
+    path('products', url.products),
 ]
