26cea1687477aa5caf989a261dca5e246bc05688
truart <trubinart@gmail.com>
2021-02-07 12:11:18 +0300

    hw_6 done

--- basketapp/views.py
+++ basketapp/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from mainapp.models import Products
 from basketapp.models import Basket
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
-
+@login_required
 def basket_add(request, product_id):
     product = get_object_or_404(Products, id=product_id)
     baskets = Basket.objects.all().filter(user=request.user, product=product)
@@ -24,3 +27,24 @@ def basket_remove(request, id):
     basket = Basket.objects.get(id=id)
     basket.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def basket_edit(request, name, quantity):
+    if request.is_ajax():
+        quantity_2 = int(quantity)
+        new_basket = Basket.objects.get(id=int(name))
+
+        if quantity_2 > 0:
+            new_basket.quantity = quantity_2
+            new_basket.save()
+        else:
+            new_basket.delete()
+
+    basket_final = Basket.objects.filter(user=request.user)
+
+    content = {
+        'basket': basket_final
+    }
+
+    result = render_to_string('basketapp/basket.html', content)
+
+    return JsonResponse({'result': result})
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -130,3 +130,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'authapp.Users'
+
+LOGIN_URL = '/auth/login/'
