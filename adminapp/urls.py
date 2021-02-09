e5477b1f5dc3f44594cbe15fdaa587f033e05e22
truart <trubinart@gmail.com>
2021-02-09 22:27:25 +0300

    hw7 done

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('read/', views.users_read, name='read'),
     path('create/', views.users_create, name='create'),
-    path('update/', views.users_update, name='update'),
-    # path('delete/', views.users_delete, name='delete'),
+    path('update/<int:id>/', views.users_update, name='update'),
+    path('delete/<int:id>/', views.users_delete, name='delete'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,23 +1,59 @@
 from django.shortcuts import render, HttpResponseRedirect
-from authapp.forms import UsersLoginForm, UsersRegistration, UsersProfileForm
-from django.contrib import auth
 from django.urls import reverse
-from basketapp.models import Basket
-from django.contrib.auth.decorators import login_required
+from authapp.models import Users
+from adminapp.forms import UserAdminRegistration, UserAdminUpdate
+from django.contrib.auth.decorators import user_passes_test
 
+
+@user_passes_test(lambda user: user.is_active)
 def index(request):
     return render(request, 'adminapp/admin.html')
 
+
+@user_passes_test(lambda user: user.is_active)
 def users_create(request):
-    return render(request, 'adminapp/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegistration(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('adminapp:read'))
+    else:
+        form = UserAdminRegistration()
+    content = {
+        'form': form
+    }
+    return render(request, 'adminapp/admin-users-create.html', content)
 
 
+@user_passes_test(lambda user: user.is_active)
 def users_read(request):
-    return render(request, 'adminapp/admin-users-read.html')
+    content = {
+        'users': Users.objects.all()
+    }
+    return render(request, 'adminapp/admin-users-read.html', content)
 
 
-def users_update(request):
-    return render(request, 'adminapp/admin-users-update-delete.html')
+@user_passes_test(lambda user: user.is_active)
+def users_update(request, id):
+    user = Users.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminUpdate(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('adminapp:read'))
+    else:
+        form = UserAdminUpdate(instance=user)
 
-def users_delete(request):
-    pass
+    content = {
+        'form': form,
+        'current_user': user
+    }
+    return render(request, 'adminapp/admin-users-update-delete.html', content)
+
+
+@user_passes_test(lambda user: user.is_active)
+def users_delete(request, id):
+    user = Users.objects.get(id=id)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reverse('adminapp:read'))
