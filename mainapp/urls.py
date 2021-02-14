72addf52e23637cafdd0903856747a79415811fe
truart <trubinart@gmail.com>
2021-02-14 09:51:23 +0300

    add paginator

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,5 @@ app_name = 'mainapp'
 urlpatterns = [
    path('', products, name='index'),
    path('<int:category_id>/', products, name='product'),
+   path('page/<int:page>/', products, name='page'),
 ]
